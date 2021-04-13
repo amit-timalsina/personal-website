2a818170be01d79e4fac40b722db0517055d7937
Amit Timalsina <amittimalsina14@gmail.com>
2021-04-13 09:53:49 +0545

    Initial Commit

---
+++ base/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'base/index.html')
+
+def posts(request):
+    return render(request, 'base/posts.html')
