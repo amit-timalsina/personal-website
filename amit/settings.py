fbad067e0ba8789026a2e239a5f9d77bbc143500
Amit Timalsina <amittimalsina14@gmail.com>
2021-05-04 12:38:26 +0545

    static and media root

--- amit/settings.py
+++ amit/settings.py
@@ -130,17 +130,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = 'static/images/'
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -149,7 +148,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'tempmail.ysc@gmail.com'
 EMAIL_HOST_PASSWORD = 'yscrecast'
 
-CKEDITOR_UPLOAD_PATH = 'uploads/'
+CKEDITOR_UPLOAD_PATH = 'static/images/uploads/'
 
 CKEDITOR_CONFIGS = {
     'default': {
